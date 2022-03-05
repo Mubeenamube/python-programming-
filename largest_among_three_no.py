0d478066e627c5fe556dc8662d5057b5b1a7854a
mubeena <95677268+Mubeenamube@users.noreply.github.com>
2022-03-05 11:23:30 +0530

    Add files via upload

---
+++ largest_among_three_no.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if(a>b)&(a>c):
+    print(a," is grater")
+elif(b>a)&(b>c):
+    print(b," is grater")
+else:
+    print(c," is grater")
+
